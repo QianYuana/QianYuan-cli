1861791da0967ff560dd1c4605a5908177d45774
QianYuana <1565054238@qq.com>
2024-09-14 16:44:38 +0800

    feat:添加文档打印基础架构

---
+++ template/default/src/modules/documents-print/components/print/LodopFuncs.js
@@ -0,0 +1,81 @@
+﻿//用双端口加载主JS文件Lodop.js(或CLodopfuncs.js兼容老版本)以防其中某端口被占:
+var MainJS = "CLodopfuncs.js",
+  URL_WS1 = "ws://localhost:8000/" + MainJS, //ws用8000/18000
+  URL_WS2 = "ws://localhost:18000/" + MainJS,
+  URL_HTTP1 = "http://localhost:8000/" + MainJS, //http用8000/18000
+  URL_HTTP2 = "http://localhost:18000/" + MainJS,
+  URL_HTTP3 = "https://localhost.lodop.net:8443/" + MainJS; //https用8000/8443
+
+//==检查加载成功与否，如没成功则用http(s)再试==
+//==低版本CLODOP6.561/Lodop7.043及前)用本方法==
+function checkOrTryHttp() {
+  if (window.getCLodop) return true;
+  var head =
+    document.head ||
+    document.getElementsByTagName("head")[0] ||
+    document.documentElement;
+  var JS1 = document.createElement("script"),
+    JS2 = document.createElement("script"),
+    JS3 = document.createElement("script");
+  JS1.src = URL_HTTP1;
+  JS2.src = URL_HTTP2;
+  JS3.src = URL_HTTP3;
+  JS1.onerror = function (e) {
+    if (window.location.protocol !== "https:")
+      head.insertBefore(JS2, head.firstChild);
+    else head.insertBefore(JS3, head.firstChild);
+  };
+  JS2.onerror = JS3.onerror = function () {
+    var JSelf = document.createElement("script");
+    JSelf.src = "/" + MainJS; //JSelf让其它电脑通过本机打印（仅适用CLodop自带例子）
+    document.head.insertBefore(JSelf, document.head.firstChild);
+  };
+  head.insertBefore(JS1, head.firstChild);
+}
+
+//==加载Lodop对象的主过程:==
+(function loadCLodop() {
+  if (!window.WebSocket && window.MozWebSocket)
+    window.WebSocket = window.MozWebSocket;
+  //ws方式速度快(小于200ms)且可避免CORS错误,但要求Lodop版本足够新:
+  try {
+    var WSK1 = new WebSocket(URL_WS1);
+    WSK1.onopen = function (e) {
+      setTimeout("checkOrTryHttp()", 200);
+    };
+    WSK1.onmessage = function (e) {
+      if (!window.getCLodop) eval(e.data);
+    };
+    WSK1.onerror = function (e) {
+      var WSK2 = new WebSocket(URL_WS2);
+      WSK2.onopen = function (e) {
+        setTimeout("checkOrTryHttp()", 200);
+      };
+      WSK2.onmessage = function (e) {
+        if (!window.getCLodop) eval(e.data);
+      };
+      WSK2.onerror = function (e) {
+        checkOrTryHttp();
+      };
+    };
+  } catch (e) {
+    checkOrTryHttp();
+  }
+})();
+
+//==获取LODOP对象的主过程:==
+export function getLodop(oOBJECT, oEMBED) {
+  var LODOP;
+  try {
+    LODOP = window.getCLodop();
+    if (!LODOP && document.readyState !== "complete") {
+      return;
+    }
+    //清理原例子内的object或embed元素，避免乱提示：
+    if (oEMBED && oEMBED.parentNode) oEMBED.parentNode.removeChild(oEMBED);
+    if (oOBJECT && oOBJECT.parentNode) oOBJECT.parentNode.removeChild(oOBJECT);
+    return LODOP;
+  } catch (err) {
+    return "getLodop出错:" + err;
+  }
+}
---
+++ template/default/src/modules/documents-print/components/wang-editor/index.tsx
@@ -0,0 +1,140 @@
+import React, { useState, useEffect } from "react";
+import { Editor, Toolbar } from "@wangeditor/editor-for-react";
+import {
+  IDomEditor,
+  IEditorConfig,
+  IToolbarConfig,
+  SlateElement,
+} from "@wangeditor/editor";
+import "@wangeditor/editor/dist/css/style.css";
+
+type ImageElement = SlateElement & {
+  src: string;
+  alt: string;
+  url: string;
+  href: string;
+};
+interface IProps {
+  onChange: (html: any) => void;
+}
+
+function MyEditor(props: IProps) {
+  // editor 实例
+  const [editor, setEditor] = useState<IDomEditor | null>(null); // TS 语法
+
+  // 编辑器内容
+  const [html, setHtml] = useState("<p>hello</p>");
+
+  // 模拟 ajax 请求，异步设置 html
+  useEffect(() => {
+    setTimeout(() => {
+      setHtml("<p>hello world</p>");
+    }, 1500);
+  }, []);
+
+  // 工具栏配置
+  const toolbarConfig: Partial<IToolbarConfig> = {}; // TS 语法
+
+  // 编辑器配置
+  const editorConfig: Partial<IEditorConfig> = {
+    MENU_CONF: {},
+    placeholder: "请输入内容...",
+  };
+  // 自定义校验图片
+  function customCheckImageFn(
+    src: string,
+    alt: string,
+    url: string
+  ): boolean | undefined | string {
+    // TS 语法
+    // function customCheckImageFn(src, alt, url) {                                                    // JS 语法
+    if (!src) {
+      return;
+    }
+    if (src.indexOf("http") !== 0) {
+      return "图片网址必须以 http/https 开头";
+    }
+    return true;
+
+    // 返回值有三种选择：
+    // 1. 返回 true ，说明检查通过，编辑器将正常插入图片
+    // 2. 返回一个字符串，说明检查未通过，编辑器会阻止插入。会 alert 出错误信息（即返回的字符串）
+    // 3. 返回 undefined（即没有任何返回），说明检查未通过，编辑器会阻止插入。但不会提示任何信息
+  }
+  // 转换图片链接
+  function customParseImageSrc(src: string): string {
+    // TS 语法
+    // function customParseImageSrc(src) {               // JS 语法
+    if (src.indexOf("http") !== 0) {
+      return `http://${src}`;
+    }
+    return src;
+  }
+  // 确保 MENU_CONF 已经存在
+  if (!editorConfig.MENU_CONF) {
+    editorConfig.MENU_CONF = {};
+  }
+  // 插入图片
+  editorConfig.MENU_CONF["insertImage"] = {
+    onInsertedImage(imageNode: ImageElement | null) {
+      // TS 语法
+      // onInsertedImage(imageNode) {                    // JS 语法
+      if (imageNode == null) return;
+
+      const { src, alt, url, href } = imageNode;
+      console.log("inserted image", src, alt, url, href);
+    },
+    checkImage: customCheckImageFn, // 也支持 async 函数
+    parseImageSrc: customParseImageSrc, // 也支持 async 函数
+  };
+  // 编辑图片
+  editorConfig.MENU_CONF["editImage"] = {
+    onUpdatedImage(imageNode: ImageElement | null) {
+      // TS 语法
+      // onUpdatedImage(imageNode) {                    // JS 语法
+      if (imageNode == null) return;
+
+      const { src, alt, url } = imageNode;
+      console.log("updated image", src, alt, url);
+    },
+    checkImage: customCheckImageFn, // 也支持 async 函数
+    parseImageSrc: customParseImageSrc, // 也支持 async 函数
+  };
+
+  // 及时销毁 editor ，重要！
+  useEffect(() => {
+    return () => {
+      if (editor == null) return;
+      editor.destroy();
+      setEditor(null);
+    };
+  }, [editor]);
+  useEffect(() => {
+    props.onChange(html);
+  }, [html]);
+  return (
+    <>
+      <div style={{ border: "1px solid #ccc", zIndex: 100 }}>
+        <Toolbar
+          editor={editor}
+          defaultConfig={toolbarConfig}
+          mode="default"
+          style={{ borderBottom: "1px solid #ccc" }}
+        />
+        <Editor
+          defaultConfig={editorConfig}
+          value={html}
+          onCreated={setEditor}
+          onChange={(editor) => setHtml(editor.getHtml())}
+          mode="default"
+          style={{ height: "500px", overflowY: "hidden" }}
+        />
+      </div>
+      <div style={{ marginTop: "15px", height: 168, overflow: "auto" }}>
+        {html}
+      </div>
+    </>
+  );
+}
+
+export default MyEditor;
